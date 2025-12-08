182bc54aa614cb83f9d796d7a40bf67c52f59b09
ThomasMousseau <thom.mousseau@gmail.com>
2025-12-08 13:33:01 -0500

    exp: nhid with JAX memory reduction, but it did not change the scaling laws :(

--- experiments/process_experiments.py
+++ experiments/process_experiments.py
@@ -285,8 +285,8 @@ def process_experiment_group(group_name, exp_tag, param_prefix, has_failed_exper
 if __name__ == "__main__":
 
     #! Neural Networks Experiments
-    process_experiment_group("neural-nets", "exp_mlp", "nlayers_", has_failed_experiments=True)
-    #process_experiment_group("neural-nets", "exp_mlp", "nhid_")
+    #process_experiment_group("neural-nets", "exp_mlp", "nlayers_", has_failed_experiments=True)
+    process_experiment_group("neural-nets", "exp_mlp", "nhid_")
     
     #! Environment Experiments
     # process_experiment_group("environment", "exp_env", "length_", has_failed_experiments=True)
